6235a5ec6fbfa833fad3843a7de75f1a039c4418
Patrick Sachs <kontakt@patrick-sachs.de>
2018-07-07 16:24:58 +0200

    Renamed write to send & cleaned up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const { Socket } = require("net");
 
+/**
+ * The numeric ready states of the WebSocket.
+ */
+const READY_STATE = {
+  CONNECTING: 0,
+  OPEN: 1,
+  CLOSING: 2,
+  CLOSED_: 3
+}
+
 const defaultOptions = {
   // The options for this webtcp server instance
   debug: false,
   mayConnect: () => true,
+  // Creates the connection/session object if you are using a non-default WebSocket implementation.
+  createConnection: (ws, _req) => ({
+    // Sends a JSON object over the WebSocket.
+    send: data => ws.send(JSON.stringify(data)),
+    // Checks if the socket is open. If this returns true, the server assumes that calling send will work.
+    isOpen: () => ws.readyState === READY_STATE.OPEN,
+    // Placeholder for the TCP socket. Simply set this to null unless you need to get really fancy.
+    socket: null
+  }),
   // The default options for the TCP socket
   defaultTcpOptions: {
     host: "localhost",
@@ -15,7 +34,7 @@ const defaultOptions = {
     initialDelay: 0
   }
 }
-const SOCKET = Symbol("webtcp-socket");
+const SOCKET = "socket";
 
 class WebTCP {
   constructor(options = {}) {
@@ -31,16 +50,16 @@ class WebTCP {
   }
 
   close(connection) {
-    if (connection[SOCKET]) {
+    if (connection.socket) {
       this.options.debug && console.log("[webtcp] Closing connection");
-      connection[SOCKET].end();
+      connection.socket.end();
     }
   }
 
   dispatch(connection, message) {
     try {
       const json = JSON.parse(message);
-      this.options.debug && console.log("[webtcp] Got message", json, "has socket?", !!connection[SOCKET]);
+      this.options.debug && console.log("[webtcp] Got message", json, "has socket?", !!connection.socket);
       switch (json.type) {
         case "connect": {
           this.dispatchConnect(connection, json);
@@ -58,7 +77,7 @@ class WebTCP {
     }
     catch (e) {
       console.log("error", e);
-      connection.write({
+      connection.send({
         type: "error",
         error: e.message
       });
@@ -66,10 +85,10 @@ class WebTCP {
   }
 
   dispatchClose(connection, json) {
-    if (connection[SOCKET]) {
+    if (connection.socket) {
       this.close(connection);
     } else {
-      connection.write({
+      connection.send({
         type: "error",
         error: "not connected"
       });
@@ -77,20 +96,20 @@ class WebTCP {
   }
 
   dispatchData(connection, json) {
-    if (connection[SOCKET]) {
+    if (connection.socket) {
       if (json.payload !== undefined) {
         const payload = typeof json.payload === "string"
           ? json.payload
           : Uint8Array.from(json.payload);
-        connection[SOCKET].write(payload, this.options.encoding);
+        connection.socket.write(payload, this.options.encoding);
       } else {
-        connection.write({
+        connection.send({
           type: "error",
           error: "no payload"
         });
       }
     } else {
-      connection.write({
+      connection.send({
         type: "error",
         error: "not connected"
       });
@@ -98,13 +117,13 @@ class WebTCP {
   }
 
   dispatchConnect(connection, json) {
-    if (!connection[SOCKET]) {
+    if (!connection.socket) {
       const tcpOptions = {
         ...this.options.defaultTcpOptions,
         ...json
       }
       if (this.options.mayConnect({ host: tcpOptions.host, port: tcpOptions.port })) {
-        const socket = connection[SOCKET] = new Socket();
+        const socket = connection.socket = new Socket();
         socket.connect(tcpOptions.port, tcpOptions.host, () => {
           socket.setEncoding(tcpOptions.encoding);
           socket.setTimeout(tcpOptions.timeout);
@@ -113,19 +132,19 @@ class WebTCP {
         });
         socket.on("ready", () => {
           this.options.debug && console.log("[webtcp] Socket ready");
-          connection.write({ type: "connect" });
+          connection.send({ type: "connect" });
         });
         socket.on("end", () => {
           this.options.debug && console.log("[webtcp] Socket end");
           if (connection.isOpen()) {
-            connection.write({ type: "end" });
+            connection.send({ type: "end" });
           }
         });
         socket.on("close", hadError => {
-          delete connection[SOCKET];
+          connection.socket = null;
           this.options.debug && console.log("[webtcp] Socket closed", "error?", hadError);
           if (connection.isOpen()) {
-            connection.write({
+            connection.send({
               type: "close",
               hadError
             });
@@ -134,12 +153,12 @@ class WebTCP {
         socket.on("timeout", () => {
           this.options.debug && console.log("[webtcp] Socket timeout");
           socket.destroy();
-          connection.write({ type: "timeout" });
+          connection.send({ type: "timeout" });
         });
         socket.on("error", (error) => {
           this.options.debug && console.log("[webtcp] Socket error", error);
           if (connection.isOpen()) {
-            connection.write({
+            connection.send({
               type: "error",
               error: error.errno
             });
@@ -147,20 +166,20 @@ class WebTCP {
         });
         socket.on("data", payload => {
           this.options.debug && console.log("[webtcp] Socket data", payload);
-          connection.write({
+          connection.send({
             type: "data",
             // todo: forward binary data as array!
             payload: ("string" === typeof payload) ? payload : payload.toString(tcpOptions.encoding)
           });
         });
       } else {
-        connection.write({
+        connection.send({
           type: "error",
           error: "not allowed connection"
         });
       }
     } else {
-      connection.write({
+      connection.send({
         type: "error",
         error: "already connected"
       });
@@ -168,29 +187,10 @@ class WebTCP {
   }
 
   handle(ws, req) {
-    const connection = {
-      write: data => ws.send(JSON.stringify(data)),
-      isOpen: () => ws.readyState === WebSocket.OPEN
-    };
-    console.log("handle-ws", ws, req);
+    const connection = this.options.createConnection(ws, req);
     ws.on('message', message => this.dispatch(connection, message));
     ws.on("close", () => this.close(connection));
   }
-
-  /*// todo: allow express integration
-  install() {
-    this.websocket = new WebSocket.Server(this.options.socketOptions);
-    this.websocket.on('connection', ws => {
-      const connection = {
-        write: data => ws.send(JSON.stringify(data)),
-        isOpen: () => ws.readyState === WebSocket.OPEN
-      };
-      console.log("readyState", ws.readyState)
-      ws.on('message', message => this.dispatch(connection, message));
-      ws.on("close", () => this.close(connection));
-    });
-    this.options.debug && console.log("[webtcp] Listening", this.options.socketOptions);
-  }*/
 }
 
 module.exports = WebTCP;
